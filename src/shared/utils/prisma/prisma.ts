8d894f41e7c84581a9013e7629b5763dbf4a9012
separatrix <nikitalochmatov@gmail.com>
2026-01-27 18:28:19 +0300

    fix

--- src/shared/utils/prisma/prisma.ts
+++ src/shared/utils/prisma/prisma.ts
@@ -2,7 +2,12 @@ import { PrismaClient } from "@/generated/prisma";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
+
 const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL environment variable is not set");
+}
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaPg(pool);
