2a783d3620e3248fa8292dc8d4ce146ed5b21e53
separatrix <nikitalochmatov@gmail.com>
2026-01-28 16:28:30 +0300

    feat: added rspack

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withRspack from "next-rspack";
 
 
 const nextConfig: NextConfig = {
@@ -42,4 +43,4 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default nextConfig;
+export default withRspack(nextConfig);
